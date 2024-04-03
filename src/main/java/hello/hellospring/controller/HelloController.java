0d707675372bff21ca137c786f8edef545ae2098
박범근 <qjarms568@naver.com>
2024-04-03 22:15:12 +0900

    mvc, api 예재.

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,9 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 
 
@@ -15,5 +18,38 @@ public class HelloController {
         return new String("hello");
     }
     
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    
+    @GetMapping("hello-string")
+    @ResponseBody  // http 바디부분으로 반환하겠다. 
+    public String helloString(@RequestParam(required = false) String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody  // http 바디부분으로 반환하겠다. 
+    public Hello helloApi(@RequestParam(required = false) String name) {
+        Hello hello = new Hello();
+        hello.setNeme(name);
+        return hello;
+
+    }
+
+    static class Hello {
+        private String neme;
+        public String getNeme() {
+            return neme;
+        }
+        public void setNeme(String neme) {
+            this.neme = neme;
+        }
+        
+    }
+
 
 }
